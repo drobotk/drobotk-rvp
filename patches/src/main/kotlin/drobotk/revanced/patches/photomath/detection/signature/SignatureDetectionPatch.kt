1c789f54cb829368722ef4ca728c570a2b64db3f
drobotk <pawwwll@gmail.com>
2025-03-29 13:36:10 +0100

    feat(Photomath): Add+fix patches

---
+++ patches/src/main/kotlin/drobotk/revanced/patches/photomath/detection/signature/SignatureDetectionPatch.kt
@@ -0,0 +1,19 @@
+package drobotk.revanced.patches.photomath.detection.signature
+
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.patch.bytecodePatch
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+
+val signatureDetectionPatch = bytecodePatch(
+    description = "Disables detection of incorrect signature.",
+) {
+
+    execute {
+        println(checkSignatureFingerprint.method)
+        val replacementIndex = checkSignatureFingerprint.patternMatch!!.endIndex
+        val checkRegister =
+            checkSignatureFingerprint.method.getInstruction<OneRegisterInstruction>(replacementIndex).registerA
+        checkSignatureFingerprint.method.replaceInstruction(replacementIndex, "const/4 v$checkRegister, 0x1")
+    }
+}
