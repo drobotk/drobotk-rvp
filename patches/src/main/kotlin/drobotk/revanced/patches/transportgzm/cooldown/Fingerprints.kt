a4a12521290d863a0803b60003866ee3378253d3
drobotk <pawwwll@gmail.com>
2025-04-01 17:51:01 +0200

    fix(Transport GZM): Remove ineffective `Remove cooldown` patch

--- patches/src/main/kotlin/drobotk/revanced/patches/transportgzm/cooldown/Fingerprints.kt
+++
@@ -1,7 +0,0 @@
-package drobotk.revanced.patches.transportgzm.cooldown
-
-import app.revanced.patcher.fingerprint
-
-internal val createCooldownFingerprint = fingerprint {
-    strings("cico_type_not_logged", "ARG_REMAINING_TIME")
-}
